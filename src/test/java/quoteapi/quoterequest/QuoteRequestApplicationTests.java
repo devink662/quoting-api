0d07b0863acf19ffd9180db474f869735a74f523
Devin Kemp <3929164@lowes.com>
2023-02-12 12:27:52 -0500

    init commit

---
+++ src/test/java/quoteapi/quoterequest/QuoteRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package quoteapi.quoterequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
